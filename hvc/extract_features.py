0276aa7ef26ef48475724547b84e37ee632eedef
NickleDave <nicholdav@gmail.com>
2017-01-05 11:17:46 -0500

    add to pure Python ipynb

---
+++ hvc/extract_features.py
@@ -0,0 +1,21 @@
+import sys
+from os.path import isdir,normpath
+
+with open(sys.argv[1],'r') as argv1_file:
+    argv1_txt = argv1_file.readlines()
+if len(argv1_txt) > 1:
+    raise ValueError("Directory names file longer than expected, \n"
+                     "should only be one line.\n"
+                     "Check formatting of string in cell.")
+argv1_txt = argv1_txt[0].split(",")
+dir_names = []
+for dir_name in argv1_txt:
+    if dir_name is not '':
+        putative_dir_name = normpath(dir_name)
+        if not isdir(putative_dir_name):
+            raise ValueError(
+                "{} is not recognized as a directory".format(putative_dir_name))
+        else:
+            dir_names.append(normpath(putative_dir_name))
+
+labelset = list(sys.argv[2])
