af701ae9b5c9c97ca8546157f039525feb439968
NickleDave <nicholdav@gmail.com>
2017-03-18 02:08:28 -0400

    change setup.py to reflect current package structure

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(name='Hybrid Vocal Classifier',
       version='1.0',
-      description='Python package for automated analysis of birdsong',
+      description='Python package for automated labeling of birdsong syllables',
       author='David Nicholson',
       author_email='nicholdav at gmail dot com',
       url='https://github.com/NickleDave/hybrid-vocal-classifier',
-      packages=['hvc','hvc.utils','hvc.audio','hvc.neuralnet'],
+      packages=['hvc','hvc.neuralnet'],
       )
       
