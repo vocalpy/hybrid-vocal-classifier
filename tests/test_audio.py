1652d693939d356d1f744eb7e5dbd66a507e1db6
NickleDave <nicholdav@gmail.com>
2017-03-18 11:29:43 -0400

    rename 'test' to 'experiments' so pytest won't think those scripts are code tests

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,3 +1,13 @@
+import numpy as np
+
+import hvc.audio
+import hvc.evfuncs
+
 class test_audio():
+    
     def test_spect():
-        assert 
+        dat, fs = hvc.evfuncs.load_cbin('./test_data/cbins/'
+                                        'gy6or6_baseline_240312_0811.1165.cbin')
+        assert type(dat) == np.ndarray
+        assert dat.dtype == '>i2' # should be big-endian 16 bit
+        assert type(fs) == int
