2686126d5fc8c0ae4dac5ba8f6ee36eb10c292d5
nickledave <nicholdav@gmail.com>
2017-05-12 18:37:41 -0400

    start writing tests for extract

---
+++ tests/test_extract.py
@@ -0,0 +1,26 @@
+"""
+tests extract module
+"""
+
+import os
+import glob
+
+from sklearn.externals import joblib
+
+class TestExtract:
+    import hvc
+    hvc.extract.run('./tests/config_ymls_for_testing/test_extract.config.yml')
+    # switch to test dir
+    os.chdir(TEST_DIR)
+    ftr_files = glob.glob('features_from*')
+    ftr_dicts = []
+    for ftr_file in ftr_files:
+        ftr_dicts.append(joblib.load(ftr_file))
+
+    # for each dict, make sure length of labels == num rows in features
+
+    # make sure number of features i.e. columns is constant across feature matrices
+
+    # load summary dict
+
+    # make sure rows in summary dict features == sum of rows of each ftr file features
