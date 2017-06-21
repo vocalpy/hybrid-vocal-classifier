2dcaa7f469281bdc2e2f1a6c5800c6d935df1c1e
nickledave <nicholdav@gmail.com>
2017-06-21 01:52:49 -0400

    add new tests

--- tests/test_audiofileIO.py
+++ tests/test_audiofileIO.py
@@ -1,9 +1,11 @@
+import pytest
 import numpy as np
 import hvc.audiofileIO
 
 class TestAudiofileIO:
     
-    def test_Spectrogram(self):
+    def test_Spectrogram_init(self):
+        #test whether can init a spec object
         spec = hvc.audiofileIO.Spectrogram(samp_freq=32000,
                                            nperseg=128,
                                            noverlap=32,
@@ -11,3 +13,13 @@ class TestAudiofileIO:
                                            freq_cutoffs=[1000, 5000],
                                            filter_func = 'diff',
                                            spec_func = 'scipy')
+
+        #test whether lack of samp_freq raises error
+        with pytest.raises(ValueError):
+            spec = hvc.audiofileIO.Spectrogram()
+
+        #test that def with other params raises warning
+        with pytest.warns(UserWarning):
+            spec = hvc.audiofileIO.Spectrogram(samp_freq=32000,
+                                               nperseg=512,
+                                               ref='tachibana')
