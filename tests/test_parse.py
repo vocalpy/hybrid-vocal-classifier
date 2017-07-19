99ca8b163823cbc11317035be19df7819a32ab46
nickledave <nicholdav@gmail.com>
2017-07-19 18:55:23 -0400

    parse/select mostly works, still some bugfixes + features to add

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -5,10 +5,14 @@ test parse module
 import pytest
 import yaml
 
-import hvc.parse.extract as extract
+import hvc.parse.extract
+import hvc.parse.select
 
 with open('./test_data/config.yaml/test_parse_extract.config.yml', 'r') as yaml_file:
     test_yaml_extract = yaml.load(yaml_file)
+
+with open('./test_data/config.yaml/test_parse_select.config.yml', 'r') as yaml_file:
+    test_yaml_select = yaml.load(yaml_file)
 
 with open('../hvc/parse/features.yml') as features_yml:
     VALID_FEATURES = yaml.load(features_yml)['features']
@@ -27,40 +31,40 @@ class TestParseExtract:
     def test_extract_parse(self):
         test_yaml = test_yaml_extract['test_parse']
         # test whether valid yaml parses *without* throwing error
-        extract.validate_yaml(test_yaml['valid_with_default_spect_and_seg_params'])
-        extract.validate_yaml(test_yaml['valid_with_default_spect_params'])
-        extract.validate_yaml(test_yaml['valid_with_default_segment_params'])
-        extract.validate_yaml(test_yaml['valid_test_spect_params_with_ref'])
+        hvc.parse.extract.validate_yaml(test_yaml['valid_with_default_spect_and_seg_params'])
+        hvc.parse.extract.validate_yaml(test_yaml['valid_with_default_spect_params'])
+        hvc.parse.extract.validate_yaml(test_yaml['valid_with_default_segment_params'])
+        hvc.parse.extract.validate_yaml(test_yaml['valid_test_spect_params_with_ref'])
 
         with pytest.raises(KeyError):
-            extract.validate_yaml(test_yaml['invalid_no_todo'])
+            hvc.parse.extract.validate_yaml(test_yaml['invalid_no_todo'])
 
         with pytest.raises(KeyError):
-            extract.validate_yaml(test_yaml['invalid_missing_spect_params'])
+            hvc.parse.extract.validate_yaml(test_yaml['invalid_missing_spect_params'])
 
         with pytest.raises(KeyError):
-            extract.validate_yaml(test_yaml['invalid_missing_segment_params'])
+            hvc.parse.extract.validate_yaml(test_yaml['invalid_missing_segment_params'])
 
     def test_validate_feature_list_and_group(self):
 
         ftr_test_yml = test_yaml_extract['test_feature_list_and_group']
 
-        str_grp = extract._validate_todo_list_dict(ftr_test_yml[
+        str_grp = hvc.parse.extract._validate_todo_list_dict(ftr_test_yml[
                                                        'test_single_group_as_str'],
                                                    index=0)
         assert str_grp['feature_list'] == FTR_GROUPS['knn']
 
-        single_grp_list = extract._validate_todo_list_dict(ftr_test_yml[
+        single_grp_list = hvc.parse.extract._validate_todo_list_dict(ftr_test_yml[
                                                                'test_single_group_as_str'],
                                                            index=0)
         assert single_grp_list['feature_list'] == FTR_GROUPS['knn']
 
-        two_grp_list = extract._validate_todo_list_dict(ftr_test_yml[
+        two_grp_list = hvc.parse.extract._validate_todo_list_dict(ftr_test_yml[
                                                             'test_two_groups_as_list'],
                                                         index=0)
         assert two_grp_list['feature_list'] == FTR_GROUPS['knn'] + FTR_GROUPS['svm']
 
-        actual_ftr_list = extract._validate_todo_list_dict(ftr_test_yml[
+        actual_ftr_list = hvc.parse.extract._validate_todo_list_dict(ftr_test_yml[
                                                                'test_feature_list'],
                                                            index=0)
         assert actual_ftr_list['feature_list'] == FTR_GROUPS['knn']
@@ -71,20 +75,33 @@ class TestParseExtract:
 
         # raises key error when key missing
         with pytest.raises(KeyError):
-            extract.validate_segment_params(test_yaml['segparams_missing_key'])
+            hvc.parse.extract.validate_segment_params(test_yaml['segparams_missing_key'])
 
         # raises value error when 'threshold' value is not int
         with pytest.raises(ValueError):
-            extract.validate_segment_params(test_yaml['segparams_threshold_wrong_type'])
+            hvc.parse.extract.validate_segment_params(test_yaml['segparams_threshold_wrong_type'])
 
         # raises value error when 'min_syl_dur' value is not float
         with pytest.raises(ValueError):
-            extract.validate_segment_params(test_yaml['segparams_min_syl_dur_wrong_type'])
+            hvc.parse.extract.validate_segment_params(test_yaml['segparams_min_syl_dur_wrong_type'])
 
         # raises value error when 'min_silent_dur' value is not float
         with pytest.raises(ValueError):
-            extract.validate_segment_params(test_yaml['segparams_min_silent_dur_wrong_type'])
+            hvc.parse.extract.validate_segment_params(test_yaml['segparams_min_silent_dur_wrong_type'])
 
-#class TestParseSelect():
 
-#class TestParsePredict():
+class TestParseSelect:
+
+    def test_validate_model_dict(self):
+        test_yaml = test_yaml_select['test_validate_model_dict']
+        hvc.parse.select._validate_model_dict(test_yaml['valid_dict_with_feature_group'],
+                                              index=0)
+        hvc.parse.select._validate_model_dict(test_yaml['valid_dict_with_feature_list'],
+                                              index=0)
+
+        with pytest.raises(KeyError):
+            hvc.parse.select._validate_model_dict(test_yaml[
+                                                      'invalid_dict_with_feature_group_and_list'
+                                                  ], index=0)
+
+#class TestParsePredict()
