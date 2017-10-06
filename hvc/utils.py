ccf857de7a457e2b4ef07ab19f14e7164bc605d9
nickledave <nicholdav@gmail.com>
2017-10-06 13:43:34 -0400

    docstring + pep8 fixes

--- hvc/utils.py
+++ hvc/utils.py
@@ -18,6 +18,7 @@ from .randomdotorg import RandomDotOrg
 
 def timestamp():
     """timestampe for dir + file names
+
     Use to make sure each dir/file has unique identifier
     (so we don't load the wrong data with joblib because
     both data files have the same name)
@@ -29,8 +30,8 @@ def load_from_mat(fname,ftr_file_type,purpose='train'):
     """
     Loads feature files created in matlab
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     fname : string
         filename of .mat file
     ftr_file_type : {'knn','svm'}
@@ -81,31 +82,42 @@ def load_from_mat(fname,ftr_file_type,purpose='train'):
         return samples, labels, song_IDs_vec
 
 
-def filter_samples(samples,labels,labels_to_filter,song_ID_vec=None,remove=False):
+def filter_samples(samples, labels, labels_to_filter, song_ID_vec=None, remove=False):
+    """filter samples by their labels.  
+    Either removes or keeps the samples with the specified labels.
+    
+    Parameters
+    ----------
+    samples : ndarray
+        m-by-n numpy array with m rows of samples, each having n
+        columns of features
+    labels : ndarray
+        1d numpy array of m elements where each is a label that
+        corresponds to a row in samples. Expects an integer value
+    song_ID_vec : ndarray
+        vector of length m, where each element denotes which song
+        that sample m(sub i) belongs to in the samples array.
+        "None" by default. If provided, is filtered just like labels
+        and samples.
+    labels_to_filter : ndarray
+        like labels, 1d numpy array of integers like
+        labels, but filter_samples finds all indices of
+        a label that occurs in 'labels *and* in 'labels_to_filter',
+        and keeps only those, filtering out every other label
+        and filtering out the corresponding rows from samples
+        and the corresponding elements from song_ID_vec.
+        ***unless*** 'remove'=True, in which case it *removes*
+        _only_ the labels in labels_to_filter, and leaves
+        everything else. 
+   remove : bool
+        set remove=True when you want to remove only the labels in
+        labels to filter instead of keeping only those labels
+
+    Returns
+    -------
+    filtered_samples,filtered_labels,filtered_song_IDs
     """
-    filter_samples(samples,labels,labels_to_keep,song_ID_vec=None,remove=False)
-        input parameters:
-            samples -- m-by-n numpy array with m rows of samples, each having n
-                       columns of features
-            labels -- 1d numpy array of m elements where each is a label that
-                      corresponds to a row in samples. Expects an integer value
-            song_ID_vec -- vector of length m, where each element denotes which song
-                       that sample m(sub i) belongs to in the samples array.
-                       "None" by default. If provided, is filtered just like labels
-                       and samples.
-            labels_to_filter -- like labels, 1d numpy array of integers like
-                                lables, but filter_samples finds all indices of
-                                a label that occurs in 'labels *and* in 'labels_to_filter',
-                                and keeps only those, filtering out every other label
-                              and filtering out the corresponding rows from samples
-                              and the corresponding elements from song_ID_vec.
-                              ***unless*** 'remove'=True, in which case it *removes*
-                              _only_ the labels in labels_to_filter, and leaves
-                              everything else. 
-           remove -- set remove=True when you want to remove only the labels in
-                     labels to filter instead of keeping only those labels
-        returns: filtered_samples,filtered_labels,filtered_song_IDs
-    """
+
     if remove is True:
         indices = np.in1d(labels,labels_to_filter,invert=True)
     elif remove is False:
@@ -118,12 +130,13 @@ def filter_samples(samples,labels,labels_to_filter,song_ID_vec=None,remove=False
         filtered_song_IDs = song_ID_vec[indices]
         return filtered_samples, filtered_labels,filtered_song_IDs
 
+
 def filter_labels(labels,labelset):
     """
     filter_labels(labels,labelset)
     returns labels with any elements removed that are not in labelset
     """
-    labels_to_keep = np.in1d(labels,labelset) #returns boolean vector, True where label is in labelset
+    labels_to_keep = np.in1d(labels, labelset) #returns boolean vector, True where label is in labelset
     labels = labels[labels_to_keep]
     return labels
 
@@ -133,8 +146,8 @@ def grid_search_svm_rbf(X, y, C_range=np.logspace(-1, 4, 6),
     """carries out a grid search of C and gamma parameters for an RBF kernel to
     use with a support vector machine.
 
-    Arguments
-    ---------
+    Parameters
+    ----------
     X : ndarray
         m-by-n array containing m samples each with n features
     y : ndarray
@@ -169,7 +182,7 @@ def grid_search_svm_rbf(X, y, C_range=np.logspace(-1, 4, 6),
 
 def find_best_k(train_samples,train_labels,test_samples,test_labels):
     """find_best_k(train_samples,train_labels,holdout_samples,holdout_labels)
-    Estimates accuracy of k-neearest neighbors algorithm using different values
+    Estimates accuracy of k-nearest neighbors algorithm using different values
     of k on the samples in data_fname. As currently written, this function loops
     from k=1 to k=10. For each value of k, it generates 10 replicates by using
     a random two-thirds of the data as a training set and then using the other
@@ -178,18 +191,20 @@ def find_best_k(train_samples,train_labels,test_samples,test_labels):
     Note that the algorithm uses the distances weighted by 
     their inverse to determine the nearest neighbor, since I found empirically
     that the weighted distances always give slightly better accuracy.
-    
-    Arguments:
-        train_samples -- m-by-n numpy array with m rows of samples, each having n features
-        train_labels -- numpy vector of length m, where each element is a label corresponding
-                  to a row in 'samples'
-        holdout_samples, holdout_labels -- same as train_samples and train_labels except this
-                                           is the set kept separate and used to find best hyper-
-                                           -parameters
-        
-    Returns:
-        mn_scores -- vector of mean scores for each value of k
-        best_k -- value of k corresponding to max value in 'scores'
+
+    Parameters
+    ----------
+    train_samples -- m-by-n numpy array with m rows of samples, each having n features
+    train_labels -- numpy vector of length m, where each element is a label corresponding
+              to a row in 'samples'
+    holdout_samples, holdout_labels -- same as train_samples and train_labels except this
+                                       is the set kept separate and used to find best hyper-
+                                       -parameters
+
+    Returns
+    -------
+    mn_scores -- vector of mean scores for each value of k
+    best_k -- value of k corresponding to max value in 'scores'
 
     """
 
