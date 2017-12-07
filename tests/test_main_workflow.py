1b209db15870df8fcc057899f47b99ab268ca9ca
NickleDave <nicholdav@gmail.com>
2017-12-07 09:05:39 -0500

    fix path for configs in test_main_workflow

--- tests/test_main_workflow.py
+++ tests/test_main_workflow.py
@@ -14,7 +14,9 @@ from sklearn.externals import joblib
 
 import hvc
 
-configs = './test_data/config.yml/'
+configs = os.path.join(
+    os.path.dirname(__file__),
+    '/test_data/config.yml/')
 
 
 @pytest.fixture(scope='session')
