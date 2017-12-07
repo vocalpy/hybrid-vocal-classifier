c87aad46cfe58c293a5dbed1dd6a50af080e5491
NickleDave <nicholdav@gmail.com>
2017-12-07 09:10:09 -0500

    fix call to flatwindow func from flatwindow module in test_neuralnet

--- tests/test_neuralnet.py
+++ tests/test_neuralnet.py
@@ -10,8 +10,8 @@ class TestModels:
     def test_flatwindow(self):
         input_shape = (112, 256, 1)
         num_label_classes = 11
-        fw = hvc.neuralnet.models.flatwindow(input_shape,
-                                             num_label_classes)
+        fw = hvc.neuralnet.models.flatwindow.flatwindow(input_shape,
+                                                        num_label_classes)
         assert fw.output_shape == (None, num_label_classes)
         # when freq bins in original spectrogram is 112,
         # num of freq bins in "local window" layer should be 11
