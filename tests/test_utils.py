a8c71d6f2f7146fa0d92a3748e72167fd9e42f84
nickledave <nicholdav@gmail.com>
2017-12-16 15:23:50 -0500

    fix path in test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,8 @@ import pytest
 
 import hvc.utils
 
+this_file_with_path = __file__
+this_file_just_path = os.path.split(this_file_with_path)[0]
 
 @pytest.fixture(scope='session')
 def tmp_output_dir(tmpdir_factory):
@@ -19,11 +21,8 @@ def tmp_output_dir(tmpdir_factory):
 def test_fetch(tmp_output_dir):
     hvc.utils.fetch(dataset_str='sober.repo1.gy6or6.032612',  # one of the smaller .gz, ~31 MB
                     destination_path=str(tmp_output_dir))
-    compare_dir = os.path.join('.',
-                               'test_data',
-                               'cbins',
-                               'gy6or6',
-                               '032612')
+    compare_dir = os.path.join(this_file_just_path,
+                               os.path.normpath('test_data/cbins/gy6or6/032612'))
     os.chdir(compare_dir)
     test_data_032612 = glob('gy6or6*')
     test_data_fetched = os.listdir(os.path.join(str(tmp_output_dir),
