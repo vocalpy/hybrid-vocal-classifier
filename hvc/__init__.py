af2222d8e4e09c6a60d3b358d9ed4794e4f5d387
nickledave <nicholdav@gmail.com>
2018-02-04 00:04:53 -0500

    hvc.__init__ imports metrics

--- hvc/__init__.py
+++ hvc/__init__.py
@@ -2,8 +2,9 @@
 __init__.py imports key functions from modules to package level
 """
 
-from hvc.utils.features import load_feature_file
+from .utils.features import load_feature_file
 from .featureextract import extract
 from .labelpredict import predict
 from .modelselect import select
 from .parseconfig import parse_config
+from . import metrics
