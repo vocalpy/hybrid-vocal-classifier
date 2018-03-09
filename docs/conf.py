e1f7facc0ae02a7c29a5d9ce2cdbd879e722dfc9
nickledave <nicholdav@gmail.com>
2018-03-08 19:54:48 -0500

    add ipython directives to extensions in sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,9 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.ifconfig',
               'sphinx.ext.napoleon',
-              'numpydoc']
+              'numpydoc',
+              'IPython.sphinxext.ipython_console_highlighting',
+              'IPython.sphinxext.ipython_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
