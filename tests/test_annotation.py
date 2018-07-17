7b60bc7f2ec451911fcad1dab2cc5dc5ec9a99af
nickledave <nicholdav@gmail.com>
2018-07-16 23:37:08 -0400

    test_annot_list_to_csv passes, not a false positive now I hope

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -27,38 +27,48 @@ def tmp_output_dir(tmpdir_factory):
     return fn
 
 
-def test_notmat_to_annotat_dict():
+def test_notmat_to_annot_dict():
     notmat = os.path.join(os.path.dirname(__file__),
                           os.path.normpath(
                               'test_data/cbins/gy6or6/032412/'
                               'gy6or6_baseline_240312_0811.1165.cbin.not.mat'))
-    annot_dict = annotation.notmat_to_annotat_dict(notmat)
+    annot_dict = annotation.notmat_to_annot_dict(notmat)
     for fieldname, fieldtype in ANNOT_DICT_FIELDNAMES.items():
         assert fieldname in annot_dict
         assert type(annot_dict[fieldname]) == fieldtype
 
 
 def test_annot_list_to_csv(tmp_output_dir):
+    """compares csv created by annot_list_to_csv
+    with correctly generated csv saved in hvc/tests/test_data
+    """
     cbin_dir = os.path.join(os.path.dirname(__file__),
                             os.path.normpath(
                                 'test_data/cbins/gy6or6/032312/'))
-    notmats = glob(cbin_dir + '*.not.mat')
+    notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
+    annot_list = []
+    for notmat in notmat_list:
+        annot_list.append(annotation.notmat_to_annot_dict(notmat))
     csv_filename = os.path.join(str(tmp_output_dir),
                                 'test.csv')
-    annotation.annot_list_to_csv(notmats,
-                                 csv_filename)
+    # below, set basename to True so we can easily run tests on any system without
+    # worrying about where audio files are relative to root of directory tree
+    annotation.annot_list_to_csv(annot_list,
+                                 csv_filename,
+                                 basename=True)
     assert os.path.isfile(csv_filename)
     test_rows = []
     with open(csv_filename, 'r', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=SYL_DICT_FIELDNAMES)
+        reader = csv.reader(csvfile)
         for row in reader:
             test_rows.append(row)
+    
     csv_to_compare_with = os.path.join(os.path.dirname(__file__),
                                        os.path.normpath(
                                            'test_data/csv/gy6or6_032312.csv'))
     compare_rows = []
     with open(csv_to_compare_with, 'r', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=SYL_DICT_FIELDNAMES)
+        reader = csv.reader(csvfile)
         for row in reader:
             compare_rows.append(row)
     for test_row, compare_row in zip(test_rows, compare_rows):
@@ -73,10 +83,10 @@ def test_notmat_list_to_csv(tmp_output_dir):
     cbin_dir = os.path.join(os.path.dirname(__file__),
                             os.path.normpath(
                                 'test_data/cbins/gy6or6/032312/'))
-    notmats = glob(cbin_dir + '*.not.mat')
+    notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
     csv_filename = os.path.join(str(tmp_output_dir),
                                 'test.csv')
-    annotation.notmat_list_to_csv(notmats, csv_filename)
+    annotation.notmat_list_to_csv(notmat_list, csv_filename)
     # make sure file was created
     assert os.path.isfile(csv_filename)
 
@@ -87,7 +97,7 @@ def test_notmat_list_to_csv(tmp_output_dir):
         reader = csv.DictReader(csvfile, fieldnames=SYL_DICT_FIELDNAMES)
         header = next(reader)
         for row in reader:
-            filenames_from_csv.append(row[0])
+            filenames_from_csv.append(row['filename'])
     for fname_from_csv in filenames_from_csv:
         assert(fname_from_csv + '.not.mat' in notmats)
 
@@ -100,5 +110,17 @@ def test_load_annotation_csv():
     pass
 
 
-def test_csv_to_list():
-    pass
+def test_csv_to_annot_list():
+    csv_fname = os.path.join(os.path.dirname(__file__),
+                             os.path.normpath(
+                                 'test_data/csv/gy6or6_032312.csv'))
+    annot_list = annotation.csv_to_annot_list(csv_fname)
+    cbin_dir = os.path.join(os.path.dirname(__file__),
+                            os.path.normpath(
+                                'test_data/cbins/gy6or6/032312/'))
+    notmat_list = glob(os.path.join(cbin_dir, '*.not.mat'))
+    annot_list_to_compare_with = []
+    for notmat in notmat_list:
+        annot_list_to_compare_with.append(annotation.notmat_to_annot_dict(notmat))
+    for from_csv, from_notmat in zip(annot_list, annot_list_to_compare_with):
+        assert from_csv == from_notmat
