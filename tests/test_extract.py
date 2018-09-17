5df341c93682d87de2a96d4e31e02b0e19842c88
NickleDave <nickledave@users.noreply.github.com>
2018-09-17 16:01:21 -0400

    add test for hvc.extract using annotation file

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -10,7 +10,7 @@ import pytest
 from sklearn.externals import joblib
 
 import hvc
-
+from hvc.utils import annotation
 
 configs = os.path.join(
     os.path.dirname(__file__),
@@ -32,7 +32,7 @@ class TestExtract:
             'test_data/cbins/gy6or6/032412']
         data_dirs = [
             os.path.join(os.path.dirname(__file__),
-                         data_dir)
+                         os.path.normpath(data_dir))
             for data_dir in data_dirs
         ]
 
@@ -42,6 +42,42 @@ class TestExtract:
         return_features = True
         ftrs = hvc.extract(data_dirs=data_dirs,
                            file_format=file_format,
+                           labels_to_use=labels_to_use,
+                           feature_group=feature_group,
+                           output_dir=str(tmp_output_dir),
+                           return_features=return_features)
+        assert type(ftrs) == dict
+        assert sorted(ftrs.keys()) == ['features', 'labels']
+
+    def test_annotation_file_cbins(self, tmp_output_dir):
+        """test that calling extract doesn't fail when we
+        pass a data_dirs list that contain cbin audio files"""
+        cbin_dirs = [
+            'test_data/cbins/gy6or6/032312',
+            'test_data/cbins/gy6or6/032412']
+        cbin_dirs = [
+            os.path.join(os.path.dirname(__file__),
+                         os.path.normpath(cbin_dir))
+            for cbin_dir in cbin_dirs
+        ]
+
+        notmat_list = []
+        for cbin_dir in cbin_dirs:
+            notmat_list.extend(
+                glob(os.path.join(cbin_dir, '*.not.mat'))
+            )
+        # below, sorted() so it's the same order on different platforms
+        notmat_list = sorted(notmat_list)
+        csv_filename = os.path.join(str(tmp_output_dir),
+                                    'test.csv')
+        annotation.notmat_list_to_csv(notmat_list, csv_filename)
+
+        file_format = 'cbin'
+        labels_to_use = 'iabcdefghjk'
+        feature_group = 'knn'
+        return_features = True
+        ftrs = hvc.extract(file_format=file_format,
+                           annotation_file=csv_filename,
                            labels_to_use=labels_to_use,
                            feature_group=feature_group,
                            output_dir=str(tmp_output_dir),
