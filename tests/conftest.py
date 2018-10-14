0db91638dd8b7ea3f97cfb1869379f5b406d0df6
nickledave <nicholdav@gmail.com>
2018-10-14 12:49:25 -0400

    fix bugs in tests/conftest.py that were causing fixtures to return wrong paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
-from os.path import normpath, abspath, join
+from os.path import normpath, join, dirname
 
 import pytest
 
-this_file = abspath(__file__)
-TEST_DATA_DIR =join(this_file, normpath(abspath('test_data/')))
-HVC_SOURCE_DIR = join(this_file, normpath(abspath('../hvc/')))
+this_file_dirname = dirname(__file__)
+TEST_DATA_DIR = join(this_file_dirname, 'test_data')
+HVC_SOURCE_DIR = join(this_file_dirname, normpath('../hvc/'))
+
 
 @pytest.fixture
 def test_data_dir():
