1e2d1dfdb18265a5477a44fae4d3e19ddd95c9d4
nickledave <nicholdav@gmail.com>
2018-10-13 23:06:51 -0400

    hvc/utils/__init__.py imports annotation functions

--- hvc/utils/__init__.py
+++ hvc/utils/__init__.py
@@ -1,3 +1,5 @@
 from .general import *
 from .datasets import fetch
 from .features import load_feature_file
+from .annotation import notmat_to_annot_dict, annot_list_to_csv, notmat_list_to_csv
+from .annotation import make_notmat, xml_to_annot_list, xml_to_csv, csv_to_annot_list
