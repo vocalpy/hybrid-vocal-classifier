83abffa94a5bd90bc0daad33d7eaed4f06b3d151
nickledave <nicholdav@gmail.com>
2018-10-13 22:44:19 -0400

    add imports to hvc/__init__.py

--- hvc/__init__.py
+++ hvc/__init__.py
@@ -3,9 +3,12 @@ __init__.py imports key functions from modules to package level
 """
 
 from .utils.features import load_feature_file
+
 from .extract import extract
 from .predict import predict
 from .select import select
 from .parseconfig import parse_config
+
 from . import metrics
 from . import plot
+from . import utils
