a909c9ea32222a08e0a0da6b8866844d8e9957b2
David Nicholson <nickledave@users.noreply.github.com>
2021-04-10 22:12:48 -0400

    DEP: deprecate `neuralnet` sub-package, fix #98
    add FutureWarning in sub-package __init__.py

--- src/hvc/neuralnet/__init__.py
+++ src/hvc/neuralnet/__init__.py
@@ -1,3 +1,12 @@
 from .models import dilated
 from .models import flatwindow
 from . import utils
+
+import warnings
+
+warnings.warn(
+    "the `neuralnet` package is deprecated, and will be removed in version 0.5.0."
+    "Please consider using the library `vak` if you need neural network models: "
+    "https://github.com/NickleDave/vak",
+    FutureWarning,
+)
